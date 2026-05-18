da1582ab1aeafda5e2adbe7e57ddf6ac98e23c55
pixelskit <125912227+pixelskit@users.noreply.github.com>
2026-05-18 11:12:47 +0300

    Add activation class

---
+++ includes/class-webexpert-woocommerce-wolt-inventory-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       
+ * @since      1.0.0
+ *
+ * @package    Webexpert_Woocommerce_Wolt_Inventory
+ * @subpackage Webexpert_Woocommerce_Wolt_Inventory/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Webexpert_Woocommerce_Wolt_Inventory
+ * @subpackage Webexpert_Woocommerce_Wolt_Inventory/includes
+ * @author     PixelsKit <pixelskit-wolt>
+ */
+class Webexpert_Woocommerce_Wolt_Inventory_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
